3140e7a84485bf709c4b2fed48b14bc6e31b9b67
Rosamund <rosamund.pearce@carbonbrief.org>
2016-12-09 14:17:14 +0000

    make marker more opaque

--- script.js
+++ script.js
@@ -506,11 +506,11 @@ function getColor(d) {
 function style(feature) {
     return {
         fillColor: getColor(feature.properties.change),
-        weight: 1,
-        opacity: 0.8,
+        weight: 1.2,
+        opacity: 0.85,
         color: 'white',
-        fillOpacity: 0.85,
-			  radius: 11,
+        fillOpacity: 0.9,
+			  radius: 14,
     };
 }
 
