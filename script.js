7cc1c1640b5c8d495ce2ad3b2bc01fa2b101d92c
Rosamund <rosamund.pearce@carbonbrief.org>
2016-12-09 14:06:50 +0000

    add key and logo

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var mymap = L.map('mapid').setView([31.505, -0.09], 2);
+var mymap = L.map('mapid').setView([31.505, 7], 2);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/rospearce/ciwgju4yv00cy2pmqeggx1mx8/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoicm9zcGVhcmNlIiwiYSI6ImNpdm1sczJsZjAwOGMyeW1xNHc4ejJ0N28ifQ.4B24e0_HgfJj4sgqimETqA', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
@@ -517,7 +517,7 @@ function style(feature) {
 function onEachFeature(feature, layer) {
     // does this feature have a property named popupContent?
     if (feature.properties) {
-        layer.bindPopup('<h1>'+feature.properties.name+'</h1><p>Distance retreated: '+feature.properties.change+'m</p>', {closeButton: false, offset: L.point(0, -20)});
+        layer.bindPopup('<h1>'+feature.properties.name+'</h1>Distance retreated: '+feature.properties.change+'m<br />Period: '+feature.properties.period, {closeButton: false, offset: L.point(0, -20)});
                 layer.on('mouseover', function() { layer.openPopup(); });
                 layer.on('mouseout', function() { layer.closePopup(); });
     };
